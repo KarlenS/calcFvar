corrected Fvar formula and error calculation

CalcFvar.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
 import numpy as np
+import argparse
+import sys
 
-filename = "lcdata_420_1TeV.txt"
-#filename = "lcdata_1TeV.txt"
-#filename = "ratio.txt"
-#filename = "index.txt"
+#parsing arguments
+parser = argparse.ArgumentParser(description='Requires input file with columns: mjd, flux, mdj_err, flux_err.')
+parser.add_argument('infile', nargs='+',type=str, default="input.dat", help="Input file name")
+args = parser.parse_args()
 
-mjd,flux,fluxerr = np.loadtxt(filename,unpack=True)
+f = np.genfromtxt(args.infile[0],names=['mjd','flux','mjd_err','fluxerr'])
+
+mjd = f['mjd']
+mjd_err = f['mjd_err']
+flux = f['flux']
+fluxerr = f['fluxerr']
 
 lc_mean = flux.mean()
 lc_std = flux.std()
@@ -15,16 +22,13 @@ lc_std = flux.std()
 N = flux.size
 
 #dist_to_mean = (flux - lc_mean)
-fluxerr_sq = fluxerr**2
+fluxerr_sq = np.power(fluxerr,2)
 mse = fluxerr_sq.mean()
+var = np.power(lc_std,2)
 
-#print lc_std**2.
-#print lc_mean**2.
-#print mse
+Fvar = np.sqrt(np.abs((np.power(var,2) - mse)/np.power(lc_mean,2)))
 
-Fvar = (abs((lc_std**2. - mse)/lc_mean**2.))**0.5
+Fvar_err = (1./(2.*N)*mse**2/(lc_mean**4*Fvar**2)+mse/(N*lc_mean**2))**0.5
+#Fvar_err = (Fvar**2. + (2.*mse**2./(N*lc_mean**4.) + 4.*mse*Fvar**2./(N*lc_mean**2.))**0.5)**0.5 - Fvar
 
-Fvar_err = (Fvar**2. + (2.*mse**2./(N*lc_mean**4.) + 4.*mse*Fvar**2./(N*lc_mean**2.))**0.5)**0.5 - Fvar
-
-print Fvar
-print Fvar_err
+print Fvar, Fvar_err